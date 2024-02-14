9765bf81bf2f4b462fd0e7b51004f9df818678fe
ABenn17282002 <a.benn17282002@gmail.com>
2024-02-14 08:32:44 +0000

    詳細ページの取得

--- app/component/ArticleList.tsx
+++ app/component/ArticleList.tsx
@@ -15,7 +15,7 @@ const ArticleList = ({ artciles }: ArticleListProps) => {
 			{artciles.map((artcile)=>( //←mapで全JSON出力
 			<article className="flex flex-col shadow my-4" key={artcile.id}>
 				<Link href="#" className="hover:opacity-75">
-					<Image src="https://source.unsplash.com/collection/1346951/1000x500?sig=1"
+					<Image src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${artcile.id}`}
 					alt=""
 					width={1280}
 					height={300}/>
