be0a7c63f34effe8162cbf278e2067a2c054d5b7
ABenn17282002 <a.benn17282002@gmail.com>
2024-02-20 08:14:43 +0000

    Supabase初期設定

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -5,7 +5,7 @@ import DeleteButton from '../component/DeleteButton';
 
 const Article =  async ({params} :{params:{id: string }}) => {
 	const detailArticle = await getDetailArticle(params.id);
-	console.log(detailArticle);
+	// console.log(detailArticle);
 	return (
 		<div className='max-w-3xl mx-auto p-5'>
 			<Image src={`https://source.unsplash.com/collection/1346951/1000x500?sig=${detailArticle.id}`}
